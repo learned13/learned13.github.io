e9753be48e866ac9b5ce2d35981459e023cb2838
learned13 <64818371+learned13@users.noreply.github.com>
2020-05-09 12:13:56 -0700

    final (99.99% positive)
    gave up on fixed background night sky entirely

--- hide.js
+++ hide.js
@@ -64,6 +64,3 @@ function favi() {
 	var i = s % 12;
 	document.getElementById("favicon").href = "favicons/" + i + ".svg";
 }
-if(!isMobileDevice()){
-	document.getElementById("m_sky").style.display = none;
-}
